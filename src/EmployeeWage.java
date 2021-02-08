efe7310f5c6b5dbb0a53c366cc6b083876397947
shravan-kaja <shravan.kaja@gmail.com>
2021-02-08 12:19:30 +0530

    Added: UC4_CheckPartOrFullTime code

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -7,9 +7,8 @@ public class EmployeeWage {
 		int absentCounter=0;	
 		int wagePerHour=20;
 		int fullDayHours=8;
-		for(int i=0; i<20 ;i++)
+		for(int i=0; i<30 ;i++)
 		{
-		int rand=1;
 		float empCheck = Math.round(Math.random());
 		if(empCheck==0.0)
 		{
@@ -22,8 +21,7 @@ public class EmployeeWage {
 		}
 		System.out.println("No of Days Presnent = "+presentCounter);
 		System.out.println("No of Days Absent = "+absentCounter);
-		int dailyWage = wagePerHour*fullDayHours;
-		System.out.println("Full time Employee Daily Wage is : "+dailyWage);
+		
 		long empType = Math.round(Math.random());
 		int flag=1;
 		if(empType==0)
@@ -32,8 +30,20 @@ public class EmployeeWage {
 		}
 		int wagePerHourPart=8;
 		int fullDayHourPart=4;
+		
+		switch(flag) {
+		
+		case 0:
+			System.out.println("Employee is Part Time");
+		break;
+		case 1:
+			System.out.println("Employee is Full Time");
+		break;
+		}
+		int dailyWage = wagePerHour*fullDayHours;
+		System.out.println("Full time Employee Daily Wage is : "+dailyWage);
 		int dailyWagePart=wagePerHourPart*fullDayHourPart;
 		System.out.println("Part Time Employee daily Wage : "+dailyWagePart);
-	}
+		}
 
 }
