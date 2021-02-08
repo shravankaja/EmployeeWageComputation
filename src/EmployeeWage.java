3621df4fdf92f1aef6a6b63463b566f319336618
shravan-kaja <shravan.kaja@gmail.com>
2021-02-08 13:17:24 +0530

    Added: UC6_WageCalculateCondition Code

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -44,6 +44,33 @@ public class EmployeeWage {
 		System.out.println("Full time Employee Daily Wage is : "+dailyWage);
 		int dailyWagePart=wagePerHourPart*fullDayHourPart;
 		System.out.println("Part Time Employee daily Wage : "+dailyWagePart);
+		int partTimeHours=presentCounter*4;
+		int fullTimeHours=presentCounter*8;
+		int finalFullTimeWage=0;
+		int check=0;
+		if(flag==1)
+		{
+			if(fullTimeHours >=100 || presentCounter >= 20)
+			{
+				finalFullTimeWage=dailyWage*presentCounter;
+				System.out.println("Final wage of this Full time Employee : "+finalFullTimeWage);
+				check=1;
+			}
 		}
-
+		else if (flag==0)
+		{
+			if(partTimeHours >=50 || presentCounter >= 20)
+			{
+				int finalPartTimeWage=dailyWagePart*presentCounter;
+				System.out.println("Fianl Wage of this Part time employee : "+finalPartTimeWage);
+				check=1;
+				
+			}
+			
+		}
+	 if (check==0)   {
+			System.out.println("Minimum WorKing Hours or Days not reached");
+	 }	
+		
+	}
 }
