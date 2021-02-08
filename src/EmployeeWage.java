f2bdfda3eb8ca8fded69bfa8d19e352da53289a7
shravan-kaja <shravan.kaja@gmail.com>
2021-02-08 11:33:35 +0530

    Added: UC2_CalculateDailyWage code

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -5,6 +5,8 @@ public class EmployeeWage {
 		System.out.println("Welcome To Employee Wage Comptation System");
 		int presentCounter=0;
 		int absentCounter=0;
+		int wagePerHour=20;
+		int fullDayHours=8;
 		for(int i=0; i<20 ;i++)
 		{
 		int rand=1;
@@ -20,7 +22,8 @@ public class EmployeeWage {
 		}
 		System.out.println("No of Days Presnent = "+presentCounter);
 		System.out.println("No of Days Absent = "+absentCounter);
-		
+		int totalWage = (wagePerHour*fullDayHours)*presentCounter;
+		System.out.println("Total Wage is : "+totalWage);
 	}
 
 }
