20cf57498d28437d3905dcc22057779298995be4
shravan-kaja <shravan.kaja@gmail.com>
2021-02-08 11:21:50 +0530

    Added: UC1_EmployeeAttandenceCheck code

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -3,6 +3,24 @@ public class EmployeeWage {
 	public static void main(String Args[])
 	{
 		System.out.println("Welcome To Employee Wage Comptation System");
+		int presentCounter=0;
+		int absentCounter=0;
+		for(int i=0; i<20 ;i++)
+		{
+		int rand=1;
+		float empCheck = Math.round(Math.random());
+		if(empCheck==0.0)
+		{
+			presentCounter++;
+		}
+		else
+		{
+			absentCounter++;
+		}
+		}
+		System.out.println("No of Days Presnent = "+presentCounter);
+		System.out.println("No of Days Absent = "+absentCounter);
+		
 	}
 
 }
