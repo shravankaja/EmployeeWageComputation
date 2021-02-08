fb4ab41094f1a039145b3325f814c74562211286
shravan-kaja <shravan.kaja@gmail.com>
2021-02-08 11:58:02 +0530

    Added: UC3_AddPartTimeEmployeeWage code

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -4,7 +4,7 @@ public class EmployeeWage {
 	{
 		System.out.println("Welcome To Employee Wage Comptation System");
 		int presentCounter=0;
-		int absentCounter=0;
+		int absentCounter=0;	
 		int wagePerHour=20;
 		int fullDayHours=8;
 		for(int i=0; i<20 ;i++)
@@ -23,7 +23,17 @@ public class EmployeeWage {
 		System.out.println("No of Days Presnent = "+presentCounter);
 		System.out.println("No of Days Absent = "+absentCounter);
 		int dailyWage = wagePerHour*fullDayHours;
-		System.out.println("Daily Wage is : "+dailyWage);
+		System.out.println("Full time Employee Daily Wage is : "+dailyWage);
+		long empType = Math.round(Math.random());
+		int flag=1;
+		if(empType==0)
+		{
+			flag=0;
+		}
+		int wagePerHourPart=8;
+		int fullDayHourPart=4;
+		int dailyWagePart=wagePerHourPart*fullDayHourPart;
+		System.out.println("Part Time Employee daily Wage : "+dailyWagePart);
 	}
 
 }
