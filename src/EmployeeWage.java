505c17e1077c6beb3dd83381016ee1e03904040c
shravan-kaja <shravan.kaja@gmail.com>
2021-02-11 12:36:53 +0530

    Refactored: added constructor, classes and objects

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,21 +1,15 @@
 
-public class EmployeeWage {
+public class EmployeeWage
+		{
 	
-	
-	public static void main(String Args[])
-	{
-		System.out.println("Welcome To Employee Wage Comptation System");
-		Main m=new Main();
-		int resultAttandence[] = m.attandenceCheck(0, 0);
-		System.out.println("No of Days Presnent = "+resultAttandence[0]);
-		System.out.println("No of Days Absent = "+resultAttandence[1]);
-		int flag1 = m.fullAndPartTimeCheck();
-		int wages[] = m.dailyWageComputation();
-		System.out.println(wages[0]);
-		System.out.println(wages[1]);
-		m.wageCalculateCondition(resultAttandence[0],wages[0],wages[1]);
-		
-	
-	}
-}
+		public static void main( String[] args)
+				{
+					System.out.println("Welcome to the Employee Wage Computation");
+					Main dmartCompany = new Main(10, 20, 30, "Dmart"); 
+					dmartCompany.calcEmpWageFor();
+					Main nikeCompany = new Main(20,40,50,"Nike");
+					nikeCompany.calcEmpWageFor();
+        
+				}
+		}
 	
--- src/Main.java
+++ src/Main.java
@@ -1,88 +1,43 @@
 
 public class Main {
-	int dailyWage=0;
-	int dailyWagePart=0;
-	public static final int WAGE_PER_HOUR=20;
-	public static final int FULL_DAY_HOURS=8;
-	public static final int WAGE_PER_HOUR_PART=8;
-	public static final int FULL_DAY_HOURS_PART=4;
-	int flag=1;
-	int presentConter=0;
-	int absentConter=0;
-	int ar[] = new int[2];
-	int[] attandenceCheck  (int presentCounter , int absentCounter)
-			{
-				for(int i=0; i<30 ;i++)
-					{
-						float empCheck = Math.round(Math.random());
-						if(empCheck==0.0)
-							{
-								presentCounter++;
-							}
-						else
-							{
-								absentCounter++;
-							}
-			}
-				return new int[] {presentCounter,absentCounter};
-			}
-	int fullAndPartTimeCheck()
-		{
-			long empType = Math.round(Math.random());
-			if(empType==0.0)
-				{
-					flag=0;
-				}
-			switch(flag) {
-				case 0:
-						System.out.println("Employee is Part Time");
-						break;
-				case 1:
-						System.out.println("Employee is Full Time");
-						break;
-		     			}
-			return flag;
-		}
-	
-	int[] dailyWageComputation()
-		{
-			int dailyWage = WAGE_PER_HOUR*FULL_DAY_HOURS;
-			int dailyWagePart=WAGE_PER_HOUR_PART*FULL_DAY_HOURS_PART;
-			System.out.println("Full Time Employee Wage is : "+dailyWage);
-			System.out.println("Full Time Employee Wage is : "+dailyWagePart);
-			return new int[] {dailyWage,dailyWagePart}; 
-		}
-	
-	void wageCalculateCondition(int presentCounter,int dailyWage, int dailyWagePart)
-		{
-			int partTimeHours=presentCounter*4;
-			int fullTimeHours=presentCounter*8;
-			int finalFullTimeWage=0;
-			int check=0;
-			if(flag==1)
-				{
-					if(fullTimeHours >=100 || presentCounter >= 20)
-						{
-							finalFullTimeWage=dailyWage*presentCounter;
-							System.out.println("Final wage of this Full time Employee : "+finalFullTimeWage);
-							check=1;
-						}
-				}
-			else if ( flag == 0)
-				{
-					if(partTimeHours >=50 || presentCounter >= 20)
-						{
-							int finalPartTimeWage=dailyWagePart*presentCounter;
-							System.out.println("Fianl Wage of this Part time employee : "+finalPartTimeWage);
-							check=1;
-				
-						}
-			
-				}
-			if (check==0)   
-				{
-					System.out.println("Minimum WorKing Hours or Days not reached");
-				}	
 
-	}
+	final static int IS_FULL_TIME = 1, IS_PART_TIME = 2;
+	private final int EMP_RATE_PER_HR ;
+	private final int NUM_OF_WORKING_DAYS;
+	private final int MAX_HRS_IN_MONTH;
+	public final String COMPANY_NAME;
+	
+	public Main(int emp_hrs, int work_hrs, int hrs_month, String company)
+				{
+					this.EMP_RATE_PER_HR = emp_hrs;
+					this.NUM_OF_WORKING_DAYS = work_hrs;
+					this.MAX_HRS_IN_MONTH = hrs_month;
+					this.COMPANY_NAME = company;
+				}
+	public  void calcEmpWageFor()
+				{
+        			int totalEmpWage = 0, empHrs = 0, empWage = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        			while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+                   				totalWorkingDays++;
+                   	int empCheck = (int) (Math.random() * 10) % 3;
+                   	switch (empCheck) {
+            							case 1:
+            									empHrs = 8;
+            										 break;
+            							case 2:
+            									 empHrs = 4;
+            										  break;
+            							default:
+            									 empWage = 0;
+            				          }
+                   	totalEmpHrs += empHrs;
+                   	System.out.println("\nDAY#: "+  totalWorkingDays + "EmpHr:" + empHrs);
+        		}
+        			totalEmpWage = totalEmpHrs +  EMP_RATE_PER_HR;
+    
+        			System.out.println(COMPANY_NAME+" Total Employee Wage : "+ totalEmpWage);
+
+				}
+	
 }
+
