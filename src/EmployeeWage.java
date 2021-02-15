6854b504e98e28803765dd50f7ea9be54ebca84b
shravan-kaja <shravan.kaja@gmail.com>
2021-02-15 21:04:28 +0530

    Added: class objects array to deal with multiple companies

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,11 +1,26 @@
+import java.util.*;
+
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Employee Wage Computation");
-		Main dmartCompany = new Main(10, 20, 30, "Dmart");
-		dmartCompany.calcEmpWageFor();
-		Main nikeCompany = new Main(20, 40, 50, "Nike");
-		nikeCompany.calcEmpWageFor();
-		System.out.println(nikeCompany);
+		Main companies_List[] = new Main[10];
+
+		System.out.println("Enter number of companies");
+		Scanner sc = new Scanner(System.in);
+		int no_Of_Companies = sc.nextInt();
+		for (int i = 0; i < no_Of_Companies; i++) {
+			System.out.println("Enter employee hrs");
+			int emp_Hrs = sc.nextInt();
+			System.out.println("Enter work hours");
+			int wrk_Hrs = sc.nextInt();
+			System.out.println("Enter hours per month");
+			int hrs_Month = sc.nextInt();
+			System.out.println("Enter Company name");
+			String cmp_Name = sc.next();
+			companies_List[i] = new Main(emp_Hrs, wrk_Hrs, hrs_Month, cmp_Name);
+			companies_List[i].calcEmpWageFor();
+			companies_List[i].toString();
+		}
 	}
 }
